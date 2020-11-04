647efda7ed2057ea7fc612be651719d907246955
XandyGomes <x3.computadores@hotmail.com>
2020-11-04 13:14:18 -0300

    04.11.2020

--- frontend/src/config/store.js
+++ frontend/src/config/store.js
@@ -6,6 +6,10 @@ Vue.use(Vuex)
 export default new Vuex.Store({
     state: {
         isMenuVisible: true,
+        user: {
+            name: 'Alexandre',
+            email: 'alexandre@fatec.gov.br'
+        }
     },
     mutations: {
         toggleMenu(state, isVisible) {
